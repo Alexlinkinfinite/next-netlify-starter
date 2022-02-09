f911440d5c2ad2fe9be96ab68d9474323ee8fdd8
Alexlinkinfinite <57055365+Alexlinkinfinite@users.noreply.github.com>
2022-02-09 19:59:32 +0100

    save

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>my programing jurney</title>
+        <link rel="icon" href="/Sora-Meliae-Matrilineare-Mimes-image-x-ico.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my blog" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
