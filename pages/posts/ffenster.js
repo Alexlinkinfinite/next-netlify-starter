d33c92d4f8ede013a4e4e90ee1f23540b3afe7cb
Alexlinkinfinite <57055365+Alexlinkinfinite@users.noreply.github.com>
2022-02-15 07:58:22 +0100

    save

---
+++ pages/posts/ffenster.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Link from 'next/link'
+
+export default function SeccondPost() {
+  return (
+    <>
+      <h1>FFenster</h1>
+      <h2>i made this post to show people that i own it</h2>
+      <h2></h2>
+      <h4>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h4>
+    </>
+  )
+}
